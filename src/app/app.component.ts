aedd998ff34fb799c8ed7903cb4926eb7cfac3a9
Shree Devi Yadam <shree.yadam@gmail.com>
2021-11-05 21:14:18 -0400

    completed assignement 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'assignment3';
+  paragraph: string = "Secret Password = tuna";
+  isDisplay: boolean = false;
+  arrCount: number[] = [];
+
+  onClickDisplay() {
+    this.isDisplay = !this.isDisplay;
+    if(this.arrCount.length === 0) {
+      this.arrCount.push(1);
+    } else {
+      this.arrCount.push(this.arrCount[this.arrCount.length - 1] + 1);
+    }
+  }
+
+  getColor(num) {
+    if(num >= 5) {
+      return "blue";
+    }
+  }
 }
